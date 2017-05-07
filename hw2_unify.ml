0370b7345172a4aeb5071f6d033fd8c5b70ceca4
- <dshtuk@mail.ru>
2017-05-07 13:50:25 +0300

    Initial release

---
+++ hw2_unify.ml
@@ -0,0 +1,6 @@
+type algebraic_term = Var of string | Fun of string * (algebraic_term list)
+
+let system_to_equation x = failwith "Not implemented";;
+let apply_substitution x y = failwith "Not implemented";;
+let check_solution x y = failwith "Not implemented";;
+let solve_system x = failwith "Not implemented";;
