3d6b267412519daa5828850d2b7b6bd62c676c1d
Dmitry Shtukenberg <dshtuk@mail.ru>
2017-12-31 01:45:02 +0300

    Encoding corrected

--- hw1.ml
+++ hw1.ml
@@ -1,4 +1,4 @@
-type peano = Z | S of peano;; (* типы необходимо копировать в реализацию *)
+type peano = Z | S of peano;; (* РўРёРїС‹ РЅРµРѕР±С…РѕРґРёРјРѕ РєРѕРїРёСЂРѕРІР°С‚СЊ РІ СЂРµР°Р»РёР·Р°С†РёСЋ *)
 type lambda = Var of string | Abs of string * lambda | App of lambda * lambda;;
 
 let peano_of_int x = failwith "Not implemented";;
@@ -12,9 +12,9 @@ let add x = failwith "Not implemented";;
 let sub x = failwith "Not implemented";;
 let mul x = failwith "Not implemented";;
 let power x = failwith "Not implemented";;
-                     
+
 let rev x = failwith "Not implemented";;
 let merge_sort x = failwith "Not implemented";;
-                     
+
 let string_of_lambda x = failwith "Not implemented";;
 let lambda_of_string x = failwith "Not implemented";;
