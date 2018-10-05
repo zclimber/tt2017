17560c4a5561caaaecf41d5209bf7d05f5e05932
zclimber <zclimber@debian.WORKSPACE>
2018-10-05 03:16:15 +0300

    W algorithm added

--- hw2_inference.ml
+++ hw2_inference.ml
@@ -12,11 +12,11 @@ module SS = Set.Make(String)
 
 module SM = Map.Make(String)
 
-let count = ref 0
+let s_count = ref 0
 
-let s_type () =
-  let value = !count in
-    count := value + 1;
+let s_name () =
+  let value = !s_count in
+    s_count := value + 1;
     "st" ^ string_of_int value
 
 type simp_type = 
@@ -25,7 +25,7 @@ type simp_type =
 
 let mkvar x = Hw2_unify.Var x
 
-let new_var () = mkvar (s_type ())
+let new_var () = mkvar (s_name ())
 
 let mkfun f x = Hw2_unify.Fun ("", [f;x])
 
@@ -57,7 +57,7 @@ let constraints lam =
   in collect lam frees
 
 let infer_simp_type lam =
-  count := 0;
+  s_count := 0;
   let sys, top = constraints lam in
     match Hw2_unify.solve_system sys with
       | Some solution ->
@@ -65,18 +65,114 @@ let infer_simp_type lam =
                 simp_type_of_term (Hw2_unify.apply_substitution solution top))
       | None -> None
 
+(*val algorithm_w : hm_lambda -> ((string * hm_type list) * hm_type) option*)
+
 type hm_lambda = 
     | HM_Var of string 
-    | HM_Abs of string * lambda 
-    | HM_App of lambda * lambda 
-    | HM_Let of string * lambda * lambda
+    | HM_Abs of string * hm_lambda 
+    | HM_App of hm_lambda * hm_lambda 
+    | HM_Let of string * hm_lambda * hm_lambda
 
 type hm_type = 
     | HM_Elem of string 
     | HM_Arrow of hm_type * hm_type 
     | HM_ForAll of string * hm_type
 
-(*val algorithm_w : hm_lambda -> ((string * hm_type list) * hm_type) option*)
+let hm_count = ref 0
+
+let hm_name () =
+  let value = !hm_count in
+    hm_count := value + 1;
+    "hmt" ^ string_of_int value
+
+let new_elem () = HM_Elem (hm_name ())
+
+let rec free_types hm_type =
+  match hm_type with
+    | HM_Elem(s) -> SS.singleton s
+    | HM_Arrow(x, y) -> SS.union (free_types x) (free_types y)
+    | HM_ForAll(s, x) -> SS.remove s (free_types x)
+
+let hm_subst subst in_type =
+  let rec subst_rec in_type defined =
+    match in_type with
+      | HM_Elem(s) -> if (SS.mem s defined || not (SM.mem s subst))
+          then in_type
+          else (SM.find s subst)
+      | HM_Arrow(x, y) -> 
+          HM_Arrow(subst_rec x defined, subst_rec y defined)
+      | HM_ForAll(s, x) ->
+          HM_ForAll(s, subst_rec x (SS.add s defined))
+  in subst_rec in_type SS.empty
+
+let hm_subst_map subst type_map =
+  SM.map (fun tp -> hm_subst subst tp) type_map
+
+let combine_subst subst1 subst2 =
+  let new_s2 = hm_subst_map subst1 subst2 in
+  let union tp1 tp2 =
+    match tp1 with
+      | Some s -> tp1
+      | None -> tp2
+  in
+    SM.merge (fun key tp1 tp2 -> union tp2 tp1) subst1 new_s2
+
+let rec rem_forall hm_type =
+  match hm_type with
+    | HM_ForAll(s, x) -> hm_subst (SM.singleton s (new_elem ())) (rem_forall x)
+    | _ -> hm_type
+
+let add_forall hm_type types =
+  let all_free_types = SM.fold (fun s x set -> SS.union set (free_types x)) types SS.empty in
+  let unchecked_free_types = (SS.diff (free_types hm_type) all_free_types) in
+    SS.fold (fun s x -> HM_ForAll(s, x)) unchecked_free_types hm_type
+
+exception WExc of string 
+
+let rec term_of_hm_type hm_type =
+  match hm_type with
+    | HM_Elem(a) -> Hw2_unify.Var(a)
+    | HM_Arrow(a, b) -> Hw2_unify.Fun("->", [(term_of_hm_type a);(term_of_hm_type b)])
+    | _ -> failwith "Cannot convert hm_type to term"
+
+let rec hm_type_of_term term = 
+  match term with
+    | Hw2_unify.Var(a) -> HM_Elem(a)
+    | Hw2_unify.Fun(name, [l;r]) -> HM_Arrow(hm_type_of_term l, hm_type_of_term r)
+    | _ -> failwith "Cannot convert term to hm_type"
+
+let map_of_alist alist =
+  List.fold_left (fun map (key, value) -> SM.add key value map) SM.empty alist
+
+let rec alg_w_rec types hm_lam =
+  match hm_lam with
+    | HM_Var(s) -> if SM.mem s types
+        then (rem_forall (SM.find s types), SM.empty)
+        else raise (WExc "Free variable")
+    | HM_Abs(s, x) ->
+        let new_hm = new_elem () in
+        let (x_hm, x_sub) = alg_w_rec (SM.add s new_hm types) x in
+          (HM_Arrow(hm_subst x_sub new_hm, x_hm), x_sub)
+    | HM_Let(s, x, y) -> 
+        let (x_hm, x_sub) = alg_w_rec types x in
+        let new_types = hm_subst_map x_sub types in
+        let (y_hm, y_sub) = alg_w_rec (SM.add s (add_forall x_hm new_types) new_types) y in
+          (y_hm, combine_subst y_sub x_sub)
+    | HM_App(x, y) -> 
+        (let (x_hm, x_sub) = alg_w_rec types x in
+         let (y_hm, y_sub) = alg_w_rec (hm_subst_map x_sub types) y in
+         let new_hm = new_elem () in 
+           match solve_system [(term_of_hm_type (hm_subst y_sub x_hm), term_of_hm_type(HM_Arrow(y_hm, new_hm)))] with
+             | None -> raise (WExc "Unsolvable system in application")
+             | Some ans -> 
+                 let ans_sub = SM.map (hm_type_of_term) (map_of_alist ans) in
+                 let all_types = combine_subst ans_sub (combine_subst y_sub x_sub) in
+                   (hm_subst all_types new_hm, all_types) )
 
 let algorithm_w hm_lam =
-  None
+  let start_types = SM.empty
+  in try
+      let (hm, types) = alg_w_rec start_types hm_lam in
+        Some (SM.bindings types, hm)
+    with (WExc e) -> None
+
--- hw2_inference_test.ml
+++ hw2_inference_test.ml
@@ -1,4 +1,60 @@
-(*
-#load "hw2_inference.cmo"
-*)
+open Hw1
+open Hw2_unify
 open Hw2_inference
+
+type test_for_alpha_eq = {lmd1 : string; lmd2 : string; ans : bool};;
+
+let tests_fae = 
+  [{lmd1 = "(x)"; lmd2 = "(y)"; ans = false};
+   {lmd1 = "x y"; lmd2 = "x y"; ans = true};
+   {lmd1 = "\\x.x y"; lmd2 = "\\y.y y"; ans = false};
+   {lmd1 = "\\x1.x1"; lmd2 = "\\y1.y1"; ans = true};
+   {lmd1 = "(\\x.x)(z)(w)"; lmd2 = "(\\y.y)(z)(w)"; ans = true};
+   {lmd1 = "\\x1.\\x2.\\x3.\\x4.x1 x2 x3 x4";
+    lmd2 = "\\y1.\\y2.\\y3.\\y4.y1 y2 y3 y4"; ans = true};
+   {lmd1 = "\\x1.\\x2.\\x3.\\x4.x4 x2 x3 x1";
+    lmd2 = "\\y1.\\y2.\\y3.\\y4.y1 y2 y3 y4"; ans = false};
+   {lmd1 = "\\x1.\\x2.x1 x2";  lmd2 = "\\y1.\\y2.y2 y1"; ans = false}];;
+
+let rec str_of_s_t s_type = match s_type with
+  | S_Elem x -> x
+  | S_Arrow (f, x) -> "(" ^ (str_of_s_t f) ^ " -> " ^ str_of_s_t x ^ ")"
+
+let type_tester t =
+  print_string ("Alpha-eq " ^ string_of_bool t.ans ^ "\n");
+  let prnt s =
+    let res = infer_simp_type (lambda_of_string s) in
+    let res_s = match res with
+      | Some (_, st) -> str_of_s_t st
+      | None -> "no simple type"
+    in 
+      print_string (s ^ " --->\n" ^ res_s ^ "\n")
+  in prnt t.lmd1; prnt t.lmd2
+
+let rec tester pred name l ind cor incor = 
+  match l with 
+    | [] ->  print_newline()
+    | x::xs -> pred x; print_string "\n"; tester pred name xs ind cor incor;;
+
+tester (type_tester) "is_alpha_equivalent" tests_fae 1 0 0;;
+
+let rec string_of_hm_type hm_type = 
+  let rec impl a =
+    match a with
+      | HM_Elem(s)-> s
+      | HM_Arrow(x, y) -> "(" ^ impl x ^ " -> " ^ impl y ^ ")"
+      | HM_ForAll(s, y) -> "V " ^ s ^ " " ^ impl y
+  in
+    impl hm_type
+
+let test123 t = 
+  let ans1 = algorithm_w t in
+    match ans1 with 
+      | Some (l, s) -> print_string (string_of_hm_type s ^ "\n")
+      | None -> print_string ""
+
+let tt1 = HM_Abs("x", HM_Var("x"));;
+let tt2 = HM_Let("w", HM_Abs("f", HM_Abs("x", HM_App(HM_Var("f"), HM_App(HM_Var("f"), HM_Var("x"))))), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_App(HM_Var("w"), HM_Var("w"))))))))))))));; 
+
+test123 tt1;;
+test123 tt2;;
